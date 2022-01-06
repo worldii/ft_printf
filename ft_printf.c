cb6a604932ca3dfb6feb77b066e730d3475a2e75
worldii <jongha2788@u.sogang.ac.kr>
2022-01-07 01:47:44 +0900

    init3

--- ft_printf.c
+++ ft_printf.c
@@ -6,31 +6,64 @@
 /*   By: jonghapa <bbc2788@naver.com>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/06 17:37:01 by jonghapa          #+#    #+#             */
-/*   Updated: 2022/01/06 18:39:45 by jonghapa         ###   ########.fr       */
+/*   Updated: 2022/01/07 01:47:36 by jonghapa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-
-int	ft_printChar(const char c)
+int	ft_printfchar(const char c)
 {
 	ft_putchar_fd(c, 1);
 	return (1);
 }
 
-int check_type (const char c, va_list ap)
+int ft_printfstr(char *)
+{
+
+}
+
+int	ft_printfptr(unsigned long long)
+{
+
+}
+
+int	ft_printfhex(unsigned int)
+{
+
+}
+
+int ft_printfstr(char *str)
+{
+
+}
+
+int ft_printfnbr(int nbr)
+{
+
+}
+
+int ft_printfunbr(unsigned int nbr)
+{
+
+}
+
+int	check_type(const char c, va_list ap)
 {
 	if (c == 'c')
-		return (ft_printChar(va_arg(ap, int));	
+		return (ft_printfchar(va_arg(ap, int)));
 	else if (c == 's')
-		return (ft_printStr(va_arg(ap, char *));
-	else if (c == 'd')
-	else if (c == 'i')
+		return (ft_printfstr(va_arg(ap, char *)));
+	else if (c == 'd' || c == 'i')
+		return (ft_printfnbr(va_arg(ap, int)));
 	else if (c == 'u')
-	else if (c == 'x')
-	else if (c =='X')
+		return (ft_printfunbr(va_arg(ap, unsigned int)));
+	else if (c == 'x' || c == 'X')
+		return (ft_printfhex(va_arg(ap, unsigned int)));
+	else if (c == 'p')
+		return (ft_printfptr(va_arg(ap, unsigned long long)));
 }
+
 int	ft_printf(const char *format, ...)
 {
 	int		len;
@@ -44,12 +77,12 @@ int	ft_printf(const char *format, ...)
 	{
 		if (format[i] == "%")
 		{
-			len += check_type(format[i+1], ap);
+			len += check_type(format[i + 1], ap);
 			i++;
 		}
 		else
 		{
-			len += ft_printChar(format[i]);
+			len += ft_printchar(format[i]);
 		}
 	}
 	va_end(ap);
