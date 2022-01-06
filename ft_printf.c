2b6733557348e370f373128225d7b3d94cfbb8cb
worldii <jongha2788@u.sogang.ac.kr>
2022-01-06 18:39:51 +0900

    init2

--- ft_printf.c
+++ ft_printf.c
@@ -6,12 +6,31 @@
 /*   By: jonghapa <bbc2788@naver.com>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/06 17:37:01 by jonghapa          #+#    #+#             */
-/*   Updated: 2022/01/06 17:59:39 by jonghapa         ###   ########.fr       */
+/*   Updated: 2022/01/06 18:39:45 by jonghapa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
+
+int	ft_printChar(const char c)
+{
+	ft_putchar_fd(c, 1);
+	return (1);
+}
+
+int check_type (const char c, va_list ap)
+{
+	if (c == 'c')
+		return (ft_printChar(va_arg(ap, int));	
+	else if (c == 's')
+		return (ft_printStr(va_arg(ap, char *));
+	else if (c == 'd')
+	else if (c == 'i')
+	else if (c == 'u')
+	else if (c == 'x')
+	else if (c =='X')
+}
 int	ft_printf(const char *format, ...)
 {
 	int		len;
@@ -25,14 +44,14 @@ int	ft_printf(const char *format, ...)
 	{
 		if (format[i] == "%")
 		{
-
+			len += check_type(format[i+1], ap);
+			i++;
 		}
 		else
 		{
-
+			len += ft_printChar(format[i]);
 		}
 	}
-
 	va_end(ap);
 	return (len);
 }
--- ft_printf.h
+++ ft_printf.h
@@ -6,13 +6,14 @@
 /*   By: jonghapa <bbc2788@naver.com>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/06 17:48:31 by jonghapa          #+#    #+#             */
-/*   Updated: 2022/01/06 17:49:50 by jonghapa         ###   ########.fr       */
+/*   Updated: 2022/01/06 18:13:47 by jonghapa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
 
+# include "./Libft/libft.h"
 # include <stdarg.h>
 # include <unistd.h>
 # include <stdlib.h>
