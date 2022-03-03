fd3e09da91daf4ace4a5f027e43e7205a76effe9
worldii <jongha2788@u.sogang.ac.kr>
2022-03-04 07:50:29 +0900

    add README

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: jonghapa <bbc2788@naver.com>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/06 17:37:01 by jonghapa          #+#    #+#             */
-/*   Updated: 2022/01/07 01:47:36 by jonghapa         ###   ########.fr       */
+/*   Updated: 2022/01/07 01:51:17 by jonghapa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,36 +16,6 @@ int	ft_printfchar(const char c)
 {
 	ft_putchar_fd(c, 1);
 	return (1);
-}
-
-int ft_printfstr(char *)
-{
-
-}
-
-int	ft_printfptr(unsigned long long)
-{
-
-}
-
-int	ft_printfhex(unsigned int)
-{
-
-}
-
-int ft_printfstr(char *str)
-{
-
-}
-
-int ft_printfnbr(int nbr)
-{
-
-}
-
-int ft_printfunbr(unsigned int nbr)
-{
-
 }
 
 int	check_type(const char c, va_list ap)
--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: jonghapa <bbc2788@naver.com>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/06 17:48:31 by jonghapa          #+#    #+#             */
-/*   Updated: 2022/01/06 18:13:47 by jonghapa         ###   ########.fr       */
+/*   Updated: 2022/01/07 01:58:21 by jonghapa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,5 +19,13 @@
 # include <stdlib.h>
 
 int	ft_printf(const char *format, ...);
+int	check_type(const char c, va_list ap);
 
+int	ft_printfchar(const char c);
+int	ft_printfstr(char *str);
+int	ft_printfptr(unsigned long long ptr);
+int	ft_printfhex(unsigned int hex);
+int	ft_printfstr(char *str);
+int	ft_printfnbr(int nbr);
+int	ft_printfunbr(unsigned int nbr);
 #endif
